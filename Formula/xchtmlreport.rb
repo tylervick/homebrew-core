4e06e3d3d49619a237c25e2cf1fd9d759f5f119f
tvick <tvick@snapchat.com>
2021-10-08 14:08:49 -0600

    xchtmlreport 2.2.0

---
+++ Formula/xchtmlreport.rb
@@ -0,0 +1,17 @@
+class Xchtmlreport < Formula
+  desc "XCTestHTMLReport: Xcode-like HTML report for Unit and UI Tests"
+  homepage "https://github.com/XCTestHTMLReport/XCTestHTMLReport"
+  url "https://github.com/XCTestHTMLReport/XCTestHTMLReport/archive/refs/tags/2.2.0.tar.gz"
+  sha256 "bdab5934a92df2dfbeea40c3f0fefba816b48cb1c125785e87514ec0a534fada"
+  license "MIT"
+  head "https://github.com/XCTestHTMLReport/XCTestHTMLReport.git", branch: "main"
+
+  def install
+    system "swift", "build", "--disable-sandbox", "-c", "release"
+    bin.install ".build/release/xchtmlreport"
+  end
+
+  test do
+    system "true"
+  end
+end
